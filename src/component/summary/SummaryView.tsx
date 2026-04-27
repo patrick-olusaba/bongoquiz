058ef4ec024b2c51533bae3460e192ad5aa13e84
jane-spothub <techspothubke@gmail.com>
2026-04-27 12:14:31 +0300

    Add SummaryView with HTTPS proxy

--- src/component/summary/SummaryView.tsx
+++ src/component/summary/SummaryView.tsx
@@ -1,7 +1,7 @@
 export function SummaryView({ summaryId }: { summaryId: string }) {
     return (
         <iframe
-            src={`http://142.93.47.187:2027/summary/${summaryId}`}
+            src={`/api/summary/${summaryId}`}
             style={{ position: 'fixed', inset: 0, width: '100%', height: '100%', border: 'none' }}
         />
     );
